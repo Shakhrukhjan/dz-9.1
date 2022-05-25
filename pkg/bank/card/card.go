af51b2859b9f33d6cafa74881168cc503ec5a2c7
Student <Shakhrukhjane@gmail.com>
2022-05-25 21:56:33 +0300

    добавление пакеты на дз 9,2

--- pkg/bank/card/card.go
+++ pkg/bank/card/card.go
@@ -8,6 +8,7 @@ func PaymentSource(cards []types.Card) []types.PaymentSource {
 	newPay := []types.PaymentSource{}
 	for _, card := range cards {
 		if card.Active && card.Balance > 0 {
+			
 			newPay = append(newPay, types.PaymentSource{Type: "card", Number: string(card.PAN), Balance: card.Balance})
 		}
 	}
